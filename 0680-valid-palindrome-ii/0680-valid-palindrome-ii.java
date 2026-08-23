f02d87112c9f7b19269285a783264092b0cb5041
Atharva1118 <atharvag1811@gmail.com>
2026-08-23 18:26:11 +0530

    Time: 4 ms (99.04%), Space: 47.7 MB (48.47%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean PalindromeHelper(int i,int j,String S){
+        while(i<j){
+            if(S.charAt(i)!=S.charAt(j)){
+                return false;
+            }
+            i=i+1;
+            j=j-1;
+        }
+        return true;
+    }
+    public boolean validPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            char left=s.charAt(i);
+            char right=s.charAt(j);
+            if(left != right){
+                return PalindromeHelper(i+1,j,s) || PalindromeHelper(i,j-1,s);
+            }
+            else{
+                i=i+1;
+                j=j-1;
+            }
+        }
+        return true;
+    }
+}
