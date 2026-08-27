0bab9dfbc4a49a5aba4b7e256d652dd57d8857da
Atharva1118 <atharvag1811@gmail.com>
2026-08-28 00:40:06 +0530

    Time: 0 ms (100%), Space: 48.2 MB (55.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -2,16 +2,14 @@ class Solution {
     public void reverseString(char[] s) {
         int i=0;
         int j=s.length-1;
-        while (i<j){
+        while(i<j){
             char temp=s[i];
             s[i]=s[j];
             s[j]=temp;
-            i=i+1;
-            j=j-1;
+            i++;
+            j--;
+
         }
+
     }
-
-
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
 }
