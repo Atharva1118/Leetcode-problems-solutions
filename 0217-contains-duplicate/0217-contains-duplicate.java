324a6c14011368cea8f4d8c9bf03c90bad06039e
Atharva1118 <atharvag1811@gmail.com>
2026-08-27 18:18:14 +0530

    Time: 17 ms (88.41%), Space: 108.1 MB (56.74%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,29 @@
 class Solution {
+    // public boolean containsDuplicate(int[] nums) {
+    //     Arrays.sort(nums);
+    //     for(int i=0;i<nums.length-1;i++){
+            
+    //             if(nums[i]==nums[i+1]){
+    //                 return true;
+    //             }
+            
+    //     }
+    //     return false;
+    //Time Complexity: O(nlogn)
+    //Space Complexity: O(1)
+    // }
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            
-                if(nums[i]==nums[i+1]){
-                    return true;
-                }
-            
+        Set<Integer> set=new HashSet<>();
+        for(int num : nums){
+            if(set.contains(num)){
+                return true;
+            }
+            set.add(num);
         }
         return false;
+        
     }
+    // Time Complexity: O(n)
+    //Space Complexity: O(n) 
+    
 }
