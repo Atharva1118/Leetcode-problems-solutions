d92f50c8b14c750f907c3cc89fe21b9bc1fe4b2a
Atharva1118 <atharvag1811@gmail.com>
2026-08-29 11:15:09 +0530

    Time: 3 ms (59.47%), Space: 46.1 MB (99.65%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,20 +1,37 @@
 class Solution {
+    // public int[] twoSum(int[] nums, int target) {
+
+    //     //Time Complexity : O(n)
+    //     //Space Complexity: O(n)
+    //     Map<Integer,Integer> map=new HashMap<>();
+    //     for(int i=0;i<nums.length;i++){
+    //         map.put(nums[i],i);
+    //     }
+    //     for(int i=0;i<nums.length;i++){
+    //         int lookUpNumber=target-nums[i];
+    //         if(map.containsKey(lookUpNumber) && map.get(lookUpNumber)!=i){
+    //             return new int[]{
+    //                 map.get(lookUpNumber),i
+    //             };
+    //         }
+    //     }
+    //     return new int[]{-1,-1};
+    // }
+
     public int[] twoSum(int[] nums, int target) {
 
-        //Time Complexity : O(n)
-        //Space Complexity: O(n)
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-        for(int i=0;i<nums.length;i++){
-            int lookUpNumber=target-nums[i];
-            if(map.containsKey(lookUpNumber) && map.get(lookUpNumber)!=i){
+            int number=target-nums[i];
+            if(map.containsKey(number)){
                 return new int[]{
-                    map.get(lookUpNumber),i
+                    map.get(number),
+                    i
                 };
             }
+            map.put(nums[i],i);
         }
+
         return new int[]{-1,-1};
     }
 }
