f7bcb03e1bd45a97a61db4973fe817a24bec6777
Atharva1118 <atharvag1811@gmail.com>
2026-08-29 14:28:20 +0530

    Time: 27 ms (98.84%), Space: 59.1 MB (74.43%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void twoSumHelper(int[] nums,int f,List<List<Integer>> res){
+        int i=f+1;
+        int j=nums.length-1;
+        while(i<j){
+            int  sum=nums[f]+nums[i]+nums[j];
+            if(sum>0){
+                j--;
+            }else if(sum<0){
+                i++;
+            }else{
+                res.add(Arrays.asList(nums[i],nums[j],nums[f]));
+                j--;
+                i++;
+                while(i<j && nums[i]==nums[i-1]){
+                    i++;
+                }
+                while(i<j && nums[j]==nums[j+1]){
+                    j--;
+                }
+            }
+        }
+
+    }
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+        Arrays.sort(nums);
+        for(int f=0;f<nums.length;f++){
+            if(nums[f]>0){
+                break;
+            }
+            if(f==0 || nums[f]!=nums[f-1]){
+                twoSumHelper(nums,f,res);
+
+            }
+        }
+        return res;
+    }
+}
