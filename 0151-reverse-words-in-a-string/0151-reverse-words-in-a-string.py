6ea20f126c954fe13216d9f56b0163a28ae5277f
Atharva1118 <atharvag1811@gmail.com>
2026-08-29 23:42:47 +0530

    Time: 4 ms (23.54%), Space: 12.4 MB (96.22%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -1,19 +1,5 @@
 class Solution(object):
     def reverseWords(self, s):
-# text = "Python is fun to learn"
-
-# # 1. Split into a list of words
-# word_list = text.split()
-
-# # 2. Reverse the list using slicing
-# reversed_list = word_list[::-1]
-
-# # 3. Join the list back into a single string with spaces
-# reversed_text = " ".join(reversed_list)
-
-# print(reversed_text)
-# Output: "learn to fun is Python"
-
         s=s.split()
         rev=s[::-1]
         rev_string=" ".join(rev)
