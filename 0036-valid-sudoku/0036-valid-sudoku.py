bd2d3ae30a158866807a747b4ce8e95cca1d46b5
Atharva1118 <atharvag1811@gmail.com>
2026-08-30 13:52:32 +0530

    Time: 2 ms (91.03%), Space: 12.5 MB (24.2%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        rows=[[0]*9 for _ in range(9)]
+        cols=[[0]*9 for _ in range(9)]
+        boxs=[[0]*9 for _ in range(9)]
+        for r in range(9):
+            for c in range(9):
+                if board[r][c]=='.':
+                    continue
+                val=ord(board[r][c])-ord('1')
+                if rows[r][val]==1:
+                    return False
+                rows[r][val]=1
+                if cols[c][val]==1:
+                    return False
+                cols[c][val]=1
+                boxIdx=3*(r/3)+(c/3)
+                if boxs[boxIdx][val]==1:
+                    return False
+                boxs[boxIdx][val]=1
+        return True
