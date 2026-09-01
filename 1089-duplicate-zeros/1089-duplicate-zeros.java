51e0e4cbabb3127c940b3dabd5aca118dc10e02c
Atharva1118 <atharvag1811@gmail.com>
2026-09-01 17:48:54 +0530

    Time: 1 ms (99.45%), Space: 46.7 MB (80.33%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.java
+++ 1089-duplicate-zeros/1089-duplicate-zeros.java
@@ -1,33 +1,35 @@
+
 class Solution {
     public void duplicateZeros(int[] arr) {
         // Amazon, Google
-
-        int[] des = new int[arr.length];
-        int d = 0;
-
-        for (int i = 0; i < arr.length; i++) {
-
-            if (arr[i] != 0) {
-                des[d] = arr[i];
-                d++;
-            } 
-            else {
-                des[d] = 0;
-
-                if (d + 1 < des.length) {
-                    des[d + 1] = 0;
+        //Time Complexity=O(n)
+        //Space=O(n)
+        int s=0;
+        int d=0;
+        int[] dest=new int[arr.length];
+        while(s<arr.length){
+            if(arr[s]==0){
+                if (d < dest.length) {
+                    dest[d] = 0;              
                 }
-
-                d += 2;
+                d++;
+                if (d < dest.length) {
+                    dest[d] = 0;
+                    d++;
+                   
+                }
+            }else{
+                if (d < dest.length) {
+                    dest[d] = arr[s];
+                    d++;
+                }
             }
-
-            if (d >= des.length) {
-                break;
-            }
+            
+            s++;
         }
-
-        for (int i = 0; i < des.length; i++) {
-            arr[i] = des[i];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=dest[i];
         }
+        
     }
 }
