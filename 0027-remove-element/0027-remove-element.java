9d51fe5b4d1998108360c8d853316edbb9e24218
Atharva1118 <atharvag1811@gmail.com>
2026-09-04 12:23:49 +0530

    Time: 0 ms (100%), Space: 43.6 MB (43.02%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,41 @@
-class Solution {
-    public int removeElement(int[] nums, int val) {
-        
-        int k=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val){
-                nums[k]=nums[i];
+// class Solution {
+//     public int removeElement(int[] nums, int val) {
+//         //Approach 1
+//         int k=0;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]!=val){
+//                 nums[k]=nums[i];
                
-                k++;
+//                 k++;
+//             }
+//         }
+//         return k;
+//     }
+//Time Complexity=O(n)
+//Space Complexity=O(1)
+// }
+
+class Solution {
+
+    public int removeElement(int[] nums, int val) {
+
+        // Approach 2
+
+        int i = 0;
+        int j = nums.length - 1;
+
+        while (i <= j) {
+
+            if (nums[i] == val) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j--;
+            } else {
+                i++;
             }
         }
-        return k;
+
+        return j + 1;
     }
 }
