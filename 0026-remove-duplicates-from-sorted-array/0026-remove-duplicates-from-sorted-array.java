3a65bc5ff38b8e467174c6223638c0003df5e16c
Atharva1118 <atharvag1811@gmail.com>
2026-09-04 12:40:52 +0530

    Time: 0 ms (100%), Space: 46.6 MB (63.28%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,16 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-
-        int j = 1;
-
-        for(int i = 1; i < nums.length; i++) {
-
-            if(nums[i] != nums[i - 1]) {
-                nums[j] = nums[i];
-                j++;
+        //Amazon,Meta,Microsoft,BloomBerg,Google
+        int i=1;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[j-1]){
+                nums[i]=nums[j];
+                i++;
             }
         }
 
-        return j;
+        return i;
     }
 }
