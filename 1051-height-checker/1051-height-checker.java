471ae6234bfdd4d1e821d63a9b0d21d3f7b93baa
Atharva1118 <atharvag1811@gmail.com>
2026-09-07 08:02:51 +0530

    Time: 4 ms (78.35%), Space: 43.3 MB (59.84%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,17 +1,35 @@
-import java.util.Arrays;
+
+// class Solution {
+//     public int heightChecker(int[] heights) {
+//         // 1. Create a copy of the original heights array
+//         int[] expected = heights.clone(); 
+        
+//         // 2. Sort the copy in ascending order
+//         Arrays.sort(expected);
+        
+//         // 3. Compare the original vs the sorted array
+//         int count = 0;
+//         for (int i = 0; i < heights.length; i++) {
+//             if (heights[i] != expected[i]) {
+//                 count++;
+//             }
+//         }
+        
+//         return count;
+//     }
+// }
+
 
 class Solution {
     public int heightChecker(int[] heights) {
-        // 1. Create a copy of the original heights array
-        int[] expected = heights.clone(); 
-        
-        // 2. Sort the copy in ascending order
-        Arrays.sort(expected);
-        
-        // 3. Compare the original vs the sorted array
+        int[] res=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            res[i]=heights[i];
+        }
+        Arrays.sort(res);
         int count = 0;
         for (int i = 0; i < heights.length; i++) {
-            if (heights[i] != expected[i]) {
+            if (heights[i] != res[i]) {
                 count++;
             }
         }
@@ -19,3 +37,6 @@ class Solution {
         return count;
     }
 }
+//Time Complexity=O(nlogn)
+//Space Complexity=O(n)
+
