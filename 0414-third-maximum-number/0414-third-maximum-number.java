786cd1f72d77d48e9193b89a07bff17b39ca6407
Atharva1118 <atharvag1811@gmail.com>
2026-09-08 10:18:56 +0530

    Time: 3 ms (65.03%), Space: 44.8 MB (67%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -30,30 +30,67 @@
 // }
 // 
 
+// class Solution {
+//     //Google,Adobe,TikTok,Bloomberg,Uber
+//     //Time Complexity:O(nlogn)
+//     //Space Complexity:O(1)
+//     public int thirdMax(int[] nums) {
+//         Arrays.sort(nums);
+//         int i=0;
+//         int j=nums.length-1;
+//         while(i<j){
+//             int temp=nums[i];
+//             nums[i]=nums[j];
+//             nums[j]=temp;
+//             i++;
+//             j--;
+//         }
+//         int elemCount=1;
+//         for(int k=1;k<nums.length;k++){
+//             if(nums[k]!=nums[k-1]){
+//                 elemCount++;
+//             }
+//             if(elemCount==3){
+//                 return nums[k];
+//             }
+//         }
+//         return nums[0];
+
+
+        
+// }
+// }
+
 class Solution {
     //Google,Adobe,TikTok,Bloomberg,Uber
+    //Time Complexity:O(nlogn)
+    //Space Complexity:O(1)
     public int thirdMax(int[] nums) {
-        Arrays.sort(nums);
-        int i=0;
-        int j=nums.length-1;
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;
-            j--;
-        }
-        int elemCount=1;
-        for(int k=1;k<nums.length;k++){
-            if(nums[k]!=nums[k-1]){
-                elemCount++;
+        Integer max1=null;
+        Integer max2=null;
+        Integer max3=null;
+        for(int i=0;i<nums.length;i++){
+            if(max1!=null && max1==nums[i]){
+                continue;
             }
-            if(elemCount==3){
-                return nums[k];
+            if(max2!=null && max2==nums[i]){
+                continue;
+            }
+            if(max3!=null && max3==nums[i]){
+                continue;
+            }
+            if(max1==null || max1<nums[i]){
+                max3=max2;
+                max2=max1;
+                max1=nums[i];
+            }else if (max2 == null || nums[i] > max2) {
+                max3 = max2;
+                max2 = nums[i];
+            } else if (max3 == null || nums[i] > max3) {
+                max3 = nums[i];
             }
         }
-        return nums[0];
-
+    return max3 == null ? max1 : max3;
 
         
 }
