91d8fd0320e531dd0c9725446df7ebc34873a376
Atharva1118 <atharvag1811@gmail.com>
2026-09-08 22:55:10 +0530

    Time: 1 ms (99.97%), Space: 42.6 MB (63.5%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        long reversed = 0; // Use long to effortlessly detect overflow
+        
+        while (x != 0) {
+            int pop = x % 10; // Pop the last digit
+            x /= 10;          // Remove the last digit from x
+            
+            reversed = (reversed * 10) + pop; // Push digit to the reversed total
+            
+            // Check overflow conditions
+            if (reversed < Integer.MIN_VALUE || reversed > Integer.MAX_VALUE) {
+                return 0;
+            }
+        }
+        
+        return (int) reversed;
+    }
+}
