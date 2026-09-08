1f7f9c143308ef7d5b32ab2aae78d691824a5765
Atharva1118 <atharvag1811@gmail.com>
2026-09-08 09:45:13 +0530

    Time: 7 ms (29.11%), Space: 45.2 MB (32.23%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,29 +1,60 @@
+// class Solution {
+//     //Google,Adobe,TikTok,Bloomberg,Uber
+//     public int thirdMax(int[] nums) {
+//         Integer max1 = null;
+//         Integer max2 = null;
+//         Integer max3 = null;
+
+//         for (Integer num : nums) {
+//             // Skip duplicate values to ensure distinct maximums
+//             if (num.equals(max1) || num.equals(max2) || num.equals(max3)) {
+//                 continue;
+//             }
+
+//             // Shift values down as higher maximums are found
+//             if (max1 == null || num > max1) {
+//                 max3 = max2;
+//                 max2 = max1;
+//                 max1 = num;
+//             } else if (max2 == null || num > max2) {
+//                 max3 = max2;
+//                 max2 = num;
+//             } else if (max3 == null || num > max3) {
+//                 max3 = num;
+//             }
+//         }
+
+//         // If the third maximum doesn't exist, return the first maximum
+//         return max3 == null ? max1 : max3;
+//     }
+// }
+// 
+
 class Solution {
+    //Google,Adobe,TikTok,Bloomberg,Uber
     public int thirdMax(int[] nums) {
-        Integer max1 = null;
-        Integer max2 = null;
-        Integer max3 = null;
-
-        for (Integer num : nums) {
-            // Skip duplicate values to ensure distinct maximums
-            if (num.equals(max1) || num.equals(max2) || num.equals(max3)) {
-                continue;
+        Arrays.sort(nums);
+        int i=0;
+        int j=nums.length-1;
+        while(i<j){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            i++;
+            j--;
+        }
+        int elemCount=1;
+        for(int k=1;k<nums.length;k++){
+            if(nums[k]!=nums[k-1]){
+                elemCount++;
             }
-
-            // Shift values down as higher maximums are found
-            if (max1 == null || num > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = num;
-            } else if (max2 == null || num > max2) {
-                max3 = max2;
-                max2 = num;
-            } else if (max3 == null || num > max3) {
-                max3 = num;
+            if(elemCount==3){
+                return nums[k];
             }
         }
+        return nums[0];
 
-        // If the third maximum doesn't exist, return the first maximum
-        return max3 == null ? max1 : max3;
-    }
+
+        
+}
 }
