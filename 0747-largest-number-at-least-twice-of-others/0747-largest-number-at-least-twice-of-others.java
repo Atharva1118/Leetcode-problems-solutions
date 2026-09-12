fd93a061f53b2148a74c8f4dac89b8c7c93181f1
Atharva1118 <atharvag1811@gmail.com>
2026-09-12 16:34:39 +0530

    Time: 0 ms (100%), Space: 43.4 MB (19.49%) - LeetHub

--- 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
+++ 0747-largest-number-at-least-twice-of-others/0747-largest-number-at-least-twice-of-others.java
@@ -1,25 +1,52 @@
+// class Solution {
+//     public int dominantIndex(int[] nums) {
+//         //Google, Microsoft,Apple
+//         int max=-1;
+//         int idx=-1;
+//         for(int i=0;i<nums.length;i++){
+//             if(nums[i]>max){
+//                 max=nums[i];
+//                 idx=i;
+//             }
+//         }
+//         for(int i=0;i<nums.length;i++){
+//             if(max==nums[i]){
+//                 continue;
+//             }
+//             if(2*nums[i]<=max){
+//                 continue;
+//             }else{
+//                 return -1;
+//             }
+//         }
+//         return idx;
+//         //Total time complexity: O(n). 
+//         //Total space complexity: O(1).
+//     }
+// }
+
+
 class Solution {
     public int dominantIndex(int[] nums) {
         //Google, Microsoft,Apple
         int max=-1;
+        int secmax=-1;
         int idx=-1;
         for(int i=0;i<nums.length;i++){
             if(nums[i]>max){
+                secmax=max;
                 max=nums[i];
+                
                 idx=i;
+            }else if (nums[i] > secmax) {
+                secmax = nums[i]; // Update second max if it's between secmax and max
             }
         }
-        for(int i=0;i<nums.length;i++){
-            if(max==nums[i]){
-                continue;
-            }
-            if(2*nums[i]<=max){
-                continue;
-            }else{
-                return -1;
-            }
+        if(2*secmax <= max){
+            return idx;
         }
-        return idx;
-        //Total time complexity: O(n). Total space complexity: O(1).
+        return -1;
+        //Total time complexity: O(n). 
+        //Total space complexity: O(1).
     }
 }
