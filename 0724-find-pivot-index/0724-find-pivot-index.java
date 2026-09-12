7c78b2054ff7ea00d238b6015a111b83b6132e57
Atharva1118 <atharvag1811@gmail.com>
2026-09-12 11:10:53 +0530

    Time: 1 ms (96.47%), Space: 47.6 MB (8.57%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,23 +1,43 @@
+// class Solution {
+//     public int pivotIndex(int[] nums) {
+
+//         for(int i=0;i<nums.length;i++){
+//             int left_sum=0;
+//             int right_sum=0;
+//             for(int j=0;j<i;j++){
+//                 left_sum+=nums[j];
+//             }
+//             for(int j=i+1;j<nums.length;j++){
+//                 right_sum+=nums[j];
+//             }
+//             if(left_sum==right_sum){
+//                 return i;
+//             }
+//         }
+//         return -1;
+//     }
+
+//         //Time complexity: O(n^2) in the worst case. For each index i (n times), we recompute the left sum (up to i elements) and the right sum (up to n−i−1 elements), leading to roughly n(n−1)/2 operations.
+
+//         // Space complexity: O(1) extra space. It uses a few integer accumulators and does not allocate any additional data structures.
+// }
+
+
 class Solution {
     public int pivotIndex(int[] nums) {
-
+        //Meta,Radius,Copang,Microsoft,Nvidia,etc.
+        int right_sum=0;
         for(int i=0;i<nums.length;i++){
-            int left_sum=0;
-            int right_sum=0;
-            for(int j=0;j<i;j++){
-                left_sum+=nums[j];
-            }
-            for(int j=i+1;j<nums.length;j++){
-                right_sum+=nums[j];
-            }
-            if(left_sum==right_sum){
+            right_sum+=nums[i];
+        }
+        int left_sum=0;
+        for(int i=0;i<nums.length;i++){
+            if(left_sum==right_sum-nums[i]){
                 return i;
             }
+            left_sum=left_sum+nums[i];
+            right_sum=right_sum-nums[i];
         }
         return -1;
-    }
-
-        //Time complexity: O(n^2) in the worst case. For each index i (n times), we recompute the left sum (up to i elements) and the right sum (up to n−i−1 elements), leading to roughly n(n−1)/2 operations.
-
-        // Space complexity: O(1) extra space. It uses a few integer accumulators and does not allocate any additional data structures.
+    }     
 }
