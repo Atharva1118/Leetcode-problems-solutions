71db63ad57ba8dd18bfdffdf0bd244ae7ab2e742
Atharva1118 <atharvag1811@gmail.com>
2026-09-21 18:41:30 +0530

    Time: 1 ms (95.37%), Space: 43.5 MB (34.67%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        //Google,Amazon,BloomBerg,Microsoft,Apple
+        List<List<Integer>> result=new ArrayList<List<Integer>>();
+        //numsRow=5
+
+        result.add(new ArrayList<Integer>());
+        result.get(0).add(1);
+        for(int row=1;row < numRows;row++){
+            List<Integer> newRow=new ArrayList<>();
+            newRow.add(1);
+            List<Integer> prevRow=result.get(row-1);
+            
+            
+            for(int i=1;i<row;i++){
+                newRow.add(prevRow.get(i-1)+prevRow.get(i));
+            }
+            newRow.add(1);
+            result.add(newRow);
+        }
+        return result;
+    }
+}
