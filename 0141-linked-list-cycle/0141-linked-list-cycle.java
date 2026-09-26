10688633a4f1d2cdfca58316bbd73e7f42eede0c
Atharva1118 <atharvag1811@gmail.com>
2026-09-26 16:10:12 +0530

    Time: 0 ms (100%), Space: 46.8 MB (53.02%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,44 +9,44 @@
  *     }
  * }
  */
-// public class Solution {
-//     //Floyd's Hare and Tortoise Algorithm
-//     public boolean hasCycle(ListNode head) {
-//         if(head==null){
-//             return false;
-//         }
-//         ListNode slow=head;
-//         ListNode fast=head.next;
-//         while(slow!=fast){
-//             if(fast==null || fast.next==null){
-//                 return false;
-//             }
-//             fast=fast.next.next;
-//             slow=slow.next;
-//         }
-//         return true;
-//     }
-// }
-
-
 public class Solution {
+    //Floyd's Hare and Tortoise Algorithm
     public boolean hasCycle(ListNode head) {
-
-        Set<ListNode> set = new HashSet<>();
-        ListNode curr = head;
-
-        while (curr != null) {
-            if (set.contains(curr)) {
-                return true;
-            }
-
-            set.add(curr);
-            curr = curr.next;
+        if(head==null){
+            return false;
         }
-
-        return false;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(slow!=fast){
+            if(fast==null || fast.next==null){
+                return false;
+            }
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        return true;
     }
 }
+
+
+// public class Solution {
+//     public boolean hasCycle(ListNode head) {
+
+//         Set<ListNode> set = new HashSet<>();
+//         ListNode curr = head;
+
+//         while (curr != null) {
+//             if (set.contains(curr)) {
+//                 return true;
+//             }
+
+//             set.add(curr);
+//             curr = curr.next;
+//         }
+
+//         return false;
+//     }
+// }
 // Complexity
 
 // Time Complexity: O(n)
